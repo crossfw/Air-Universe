8ba0d29b906752cbc6fe93e26ff616040f930bd1
crossfw <cross2fw@gmail.com>
2021-02-08 14:16:25 +0800

    Fix problems

--- scripts/Install_single_inbound_server.sh
+++ scripts/Install_single_inbound_server.sh
@@ -2,11 +2,11 @@
 
 
 download(){
-  v2ray_url = "https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray-core/v2ray-4_34_0"
-  airuniverse_url = "https://github.com/crossfw/Air-Universe/releases/download/v0.1.0/Air-Universe-linux-amd64"
-  v2ray_json_url = "https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/v2ray-core_json/Single.json"
-  airuniverse_json_url = "https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/Air-Universe_json/Air-Universe_full.json"
-  start_script_url = "https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/Start_AU_with_v2ray.sh"
+  v2ray_url='https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray-core/v2ray-4_34_0'
+  airuniverse_url='https://github.com/crossfw/Air-Universe/releases/download/v0.1.0/Air-Universe-linux-amd64'
+  v2ray_json_url='https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/v2ray-core_json/Single.json'
+  airuniverse_json_url='https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/Air-Universe_json/Air-Universe_full.json'
+  start_script_url='https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/Start_AU_with_v2ray.sh'
   wget -N --no-check-certificate ${v2ray_url} -O ./v2
   wget -N --no-check-certificate ${v2ray_json_url} -O ./v2.json
   wget -N --no-check-certificate ${airuniverse_url} -O ./au
@@ -18,4 +18,5 @@ download(){
 }
 
 mkdir ./au
+cd au
 download;

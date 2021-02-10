83c3835e72c54e5f9a2665fb6b9a2cadbc54aade
crossfw <cross2fw@gmail.com>
2021-02-11 04:04:50 +0800

    Turnkey install script

--- scripts/Start_AU_with_v2ray.sh
+++ scripts/Start_AU_with_v2ray.sh
@@ -3,6 +3,6 @@
 #eval $(ps -ef | grep "v2 -c" | awk '{print "kill "$2}')
 #eval $(ps -ef | grep "au -c" | awk '{print "kill "$2}')
 
-nohup /usr/bin/au/v2 -c v2.json > /dev/null 2>&1 &
+nohup /usr/bin/au/v2 -c /etc/au/v2.json > /dev/null 2>&1 &
 sleep 5
-nohup /usr/bin/au/au -c au.json > /var/log/au.log 2>&1 &
+nohup /usr/bin/au/au -c /etc/au/au.json > /var/log/au.log 2>&1 &

1e296414e7548ab925a2ad12952ee60335b8c849
crossfw <cross2fw@gmail.com>
2021-02-11 12:40:36 +0800

    TurnKey install script - multi-services -> single service

--- scripts/Install_single_inbound_server.sh
+++ scripts/Install_single_inbound_server.sh
@@ -19,25 +19,29 @@ download(){
   v2ray_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray-core/v2ray-4_34_0"
   airuniverse_url="https://github.com/crossfw/Air-Universe/releases/download/v${VERSION}/Air-Universe-linux-amd64"
   v2ray_json_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/v2ray-core_json/Single.json"
+  start_script_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/Start_AU_with_v2ray.sh"
+
   wget -N --no-check-certificate ${v2ray_url} -O /usr/bin/au/v2
   wget -N --no-check-certificate ${v2ray_json_url} -O /etc/au/v2.json
   wget -N --no-check-certificate ${airuniverse_url} -O /usr/bin/au/au
+  wget -N --no-check-certificate ${start_script_url} -O /usr/bin/au/run.sh
 
   chmod +x /usr/bin/au/au
   chmod +x /usr/bin/au/v2
+  chmod +x /usr/bin/au/run.sh
 }
 
 makeConfig(){
-cat >> /etc/au/au.json << EOF
-{
-"panel": {
-  "url": "https://${pUrl}",
-  "key": "${nKey}",
-  "node_ids": [${nId}]
-},
-"proxy": {
-  "log_path": "/var/log/au-v.log"
-}
+  cat >> /etc/au/au.json << EOF
+  {
+  "panel": {
+    "url": "https://${pUrl}",
+    "key": "${nKey}",
+    "node_ids": [${nId}]
+  },
+  "proxy": {
+    "log_path": "/var/log/au-v.log"
+  }
 }
 EOF
 
@@ -45,9 +49,10 @@ sed  -i "s/11071/${vOut}/g"  /etc/au/v2.json
 
 }
 
-createV2Service(){
-cat >> /lib/systemd/system/v2.service << EOF
-[Unit]
+
+createService(){
+  cat >> /lib/systemd/system/au.service << EOF
+  [Unit]
 Description=Air-Universe Service
 After=network.target
 
@@ -56,35 +61,12 @@ Type=simple
 User=nobody
 Restart=on-failure
 RestartSec=5s
-ExecStart=/usr/bin/au/v2 -c /etc/au/v2.json
+ExecStart=/usr/bin/au/run.sh
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload;
-systemctl enable v2;
-systemctl start v2;
-}
-
-createAUService(){
-cat >> /lib/systemd/system/au.service << EOF
-[Unit]
-Description=Air-Universe Service
-After=network.target
-
-[Service]
-Type=simple
-User=nobody
-Restart=on-failure
-RestartSec=5s
-ExecStart=/usr/bin/au/au -c /etc/au/au.json
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-chmod -R 777 /var/log/
 systemctl daemon-reload;
 systemctl enable au;
 systemctl start au;
@@ -96,5 +78,5 @@ mkdir /etc/au/
 panelConfig;
 download;
 makeConfig;
-createV2Service;
-createAUService;
+createService;
+

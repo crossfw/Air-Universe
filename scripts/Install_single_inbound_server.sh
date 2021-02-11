c7a25e732bd916f9ade9befc875000edbcf38008
crossfw <cross2fw@gmail.com>
2021-02-11 13:19:40 +0800

    TurnKey install script - multi-services -> single service

--- scripts/Install_single_inbound_server.sh
+++ scripts/Install_single_inbound_server.sh
@@ -2,20 +2,18 @@
 
 VERSION="0.2.0"
 
+panelConfig() {
+  echo "########Air-Universe config#######\n"
+  read -p "Enter node_id:" nId
+  read -p "Enter sspanel domain(https://):" pUrl
+  read -p "Enter panel token:" nKey
+  read -p "Enter v2ray out port:" vOut
 
-panelConfig(){
-   echo "########Air-Universe config#######\n"
-    read -p "Enter node_id:" nId
-    read -p "Enter sspanel domain(https://):" pUrl
-    read -p "Enter panel token:" nKey
-    read -p "Enter v2ray out port:" vOut
-
-    apt-get update
-    apt-get install cron wget ca-certificates -y
+  apt-get update
+  apt-get install cron wget ca-certificates -y
 }
 
-
-download(){
+download() {
   v2ray_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray-core/v2ray-4_34_0"
   airuniverse_url="https://github.com/crossfw/Air-Universe/releases/download/v${VERSION}/Air-Universe-linux-amd64"
   v2ray_json_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/v2ray-core_json/Single.json"
@@ -25,59 +23,52 @@ download(){
   wget -N --no-check-certificate ${v2ray_json_url} -O /etc/au/v2.json
   wget -N --no-check-certificate ${airuniverse_url} -O /usr/bin/au/au
   wget -N --no-check-certificate ${start_script_url} -O /usr/bin/au/run.sh
-
-  chmod +x /usr/bin/au/au
-  chmod +x /usr/bin/au/v2
-  chmod +x /usr/bin/au/run.sh
 }
 
-makeConfig(){
-  cat >> /etc/au/au.json << EOF
+makeConfig() {
+  cat >>/etc/au/au.json <<EOF
   {
-  "panel": {
-    "url": "https://${pUrl}",
-    "key": "${nKey}",
-    "node_ids": [${nId}]
-  },
-  "proxy": {
-    "log_path": "/var/log/au-v.log"
+    "panel": {
+      "url": "https://${pUrl}",
+      "key": "${nKey}",
+      "node_ids": [${nId}]
+    },
+    "proxy": {
+      "log_path": "/var/log/au-v.log"
+    }
   }
-}
 EOF
 
-sed  -i "s/11071/${vOut}/g"  /etc/au/v2.json
+  sed -i "s/11071/${vOut}/g" /etc/au/v2.json
 
 }
 
 
-createService(){
-  cat >> /lib/systemd/system/au.service << EOF
-  [Unit]
-Description=Air-Universe Service
-After=network.target
+keepalive() {
+  cat >>/usr/bin/au/keepalive.sh <<EOF
+#!/bin/bash
+au=\$(ps -ef | grep "au -c" | grep -v grep | wc -l)
+if [ \$au -eq 0 ];then
+    bash  /usr/bin/au/run.sh restart
+else
+   echo "au ok"
+fi
 
-[Service]
-Type=simple
-User=root
-ExecStart=/usr/bin/au/run.sh start
-ExecStop=/usr/bin/au/run.sh stop
-ExecReload=/usr/bin/au/run.sh restart
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+v2=\$(ps -ef | grep "v2 -c" | grep -v grep | wc -l)
+if [ \$v2 -eq 0 ];then
+    bash  /usr/bin/au/run.sh restart
+else
+   echo "v2 ok"
+fi
 EOF
 
-systemctl daemon-reload;
-systemctl enable au;
-systemctl start au;
 }
-
 
 mkdir /usr/bin/au/
 mkdir /etc/au/
-panelConfig;
-download;
-makeConfig;
-createService;
-
+panelConfig
+download
+makeConfig
+keepalive
+chmod +x /usr/bin/au/*
+echo '*/1 * * * * /usr/bin/au/keepalive.sh'  >> /var/spool/cron/crontabs/root
--- scripts/Start_AU_with_v2ray.sh
+++ scripts/Start_AU_with_v2ray.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-start () {
-nohup /usr/bin/au/v2 -c /etc/au/v2.json > /dev/null 2>&1 &
-sleep 5
-nohup /usr/bin/au/au -c /etc/au/au.json > /var/log/au.log 2>&1 &
+start() {
+  nohup /usr/bin/au/v2 -c /etc/au/v2.json >/dev/null 2>&1 &
+  sleep 5
+  nohup /usr/bin/au/au -c /etc/au/au.json >/var/log/au.log 2>&1 &
 }
 
-stop () {
-eval $(ps -ef | grep "v2 -c" | awk '{print "kill "$2}')
-eval $(ps -ef | grep "au -c" | awk '{print "kill "$2}')
+stop() {
+  eval $(ps -ef | grep "v2 -c" | awk '{print "kill "$2}')
+  eval $(ps -ef | grep "au -c" | awk '{print "kill "$2}')
 }
 
-restart () {
-stop
-start
+restart() {
+  stop
+  start
 }
 case "$1" in
 start)
-start
-;;
+  start
+  ;;
 stop)
-stop
-;;
+  stop
+  ;;
 restart)
-stop
-start
-;;
+  stop
+  start
+  ;;
 *)
-echo "start|stop|restart"
+  echo "start|stop|restart"
+  ;;
 esac

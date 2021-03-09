691818ae4ba9254ccff398915e183880df2ec1b4
crossfw <cross2fw@gmail.com>
2021-03-09 14:11:50 +0800

    update Xray to 1.3.1; Air-Universe to 0.4.0 in turnkey script

--- scripts/xray_script/Install_server_with_xray.sh
+++ scripts/xray_script/Install_server_with_xray.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
 
-VERSION="0.3.4"
+VERSION="0.4.0"
 
 panelConfig() {
-  echo "Air-Universe 0.3.4  + Xray 1.3.0 Installation"
+  echo "Air-Universe ${VERSION}  + Xray 1.3.1 Installation"
   echo "########Air-Universe config#######\n"
-  read -p "Enter node_ids, (eg 1,2,3): " nIds
-  read -p "Enter sspanel domain(https://): " pUrl
-  read -p "Enter panel token: " nKey
+
+  read -r -p "Enter node_ids, (eg 1,2,3): " nIds
+  read -r -p "Enter sspanel domain(https://): " pUrl
+  read -r -p "Enter panel token: " nKey
 
   apt-get update
   apt-get install cron wget ca-certificates -y
 }
 
 download() {
-  v2ray_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/proxy-core/xray-1_3_0"
-  airuniverse_url="https://github.com/crossfw/Air-Universe/releases/download/v${VERSION}/Air-Universe-linux-amd64"
+  v2ray_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/proxy-core/xray-1_3_1"
+  air_universe_url="https://github.com/crossfw/Air-Universe/releases/download/v${VERSION}/Air-Universe-linux-amd64"
   v2ray_json_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/xray_json/multiIn.json"
   start_script_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/xray_script/Start_AU_with_xray.sh"
 
   wget -N --no-check-certificate ${v2ray_url} -O /usr/bin/au/xr
   wget -N --no-check-certificate ${v2ray_json_url} -O /etc/au/xr.json
-  wget -N --no-check-certificate ${airuniverse_url} -O /usr/bin/au/au
+  wget -N --no-check-certificate ${air_universe_url} -O /usr/bin/au/au
   wget -N --no-check-certificate ${start_script_url} -O /usr/bin/au/run.sh
 }
 
 makeConfig() {
-  cat >>/etc/au/au.json <<EOF
-  {
-    "panel": {
-      "url": "https://${pUrl}",
-      "key": "${nKey}",
-      "node_ids": [${nIds}]
-    },
-    "proxy": {
-      "log_path": "/var/log/xr.log"
-    }
+cat >>/etc/au/au.json <<EOF
+{
+  "panel": {
+    "url": "https://${pUrl}",
+    "key": "${nKey}",
+    "node_ids": [${nIds}]
+  },
+  "proxy": {
+    "log_path": "/var/log/xr.log"
   }
+}
 EOF
 
 }
 
 
 keepalive() {
-  cat >>/usr/bin/au/keepalive.sh <<EOF
+cat >>/usr/bin/au/keepalive.sh <<EOF
 #!/bin/bash
 au=\$(ps -ef | grep "au -c" | grep -v grep | wc -l)
 if [ \$au -eq 0 ];then

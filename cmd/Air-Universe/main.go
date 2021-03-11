e2d7419cbad411f2c05e78a73c9488a80cbab499
crossfw <cross2fw@gmail.com>
2021-03-11 17:40:35 +0800

    Add default limit policy [inf, 1.6, 24, 56, 104, 152, 200, 304, 504] Mbps

--- cmd/Air-Universe/main.go
+++ cmd/Air-Universe/main.go
@@ -32,7 +32,7 @@ func init() {
 	//log.SetReportCaller(true)
 
 	flag.BoolVar(&printVersion, "v", false, "print version")
-	flag.StringVar(&configPath, "c", "locTest/test.json", "configure file")
+	flag.StringVar(&configPath, "c", "", "configure file")
 	flag.Parse()
 
 	if printVersion {

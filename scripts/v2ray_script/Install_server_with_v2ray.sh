dcf520aaca42c8065c31992b7621784aa5bbbdb1
crossfw <cross2fw@gmail.com>
2021-03-19 16:56:58 +0800

    Turnkey install speedlimit edition for CentOS and debian

--- scripts/v2ray_script/Install_server_with_v2ray.sh
+++ scripts/v2ray_script/Install_server_with_v2ray.sh
@@ -1,76 +1,164 @@
 #!/bin/bash
 
-VERSION="0.5.1"
+VERSION=""
+APP_PATH="/usr/local/bin/"
+CONFIG_PATH="/usr/local/etc/au/"
+
+create_folders() {
+  if [[ ! -e "${APP_PATH}" ]]; then
+    mkdir "${APP_PATH}"
+  fi
+  if [[ ! -e "${CONFIG_PATH}" ]]; then
+    mkdir "${CONFIG_PATH}"
+  fi
+
+}
 
 panelConfig() {
-  echo "Air-Universe 0.5.1  + V2ray 4.32 with speedlimit Installation"
+  echo "Air-Universe $VERSION + V2ray 4.32 with speedlimit Installation"
   echo "########Air-Universe config#######\n"
   read -r -p "Enter node_ids, (eg 1,2,3): " nIds
-  read -r -p "Enter sspanel domain(https://): " pUrl
+  read -r -p "Enter sspanel domain(Include https:// or http://): " pUrl
   read -r -p "Enter panel token: " nKey
-
-  apt-get update
-  apt-get install cron wget ca-certificates -y
 }
 
+check_root() {
+  [[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
+}
+check_sys() {
+  if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+  elif cat /etc/issue | grep -q -E -i "debian"; then
+    release="debian"
+  elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+    release="ubuntu"
+  elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+    release="centos"
+  elif cat /proc/version | grep -q -E -i "debian"; then
+    release="debian"
+  elif cat /proc/version | grep -q -E -i "ubuntu"; then
+    release="ubuntu"
+  elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+    release="centos"
+  fi
+  bit=$(uname -m)
+}
+Installation_dependency() {
+  if [[ ${release} == "centos" ]]; then
+    yum update
+    yum install -y gzip ca-certificates curl
+  else
+    apt-get update
+    apt-get install -y ca-certificates curl
+  fi
+  cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+  mkdir /var/log/au
+  chown -R nobody /var/log/au
+}
 download() {
+  mkdir /usr/local/etc/au/
+
   v2ray_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/proxy-core/v2ray-speedlimit"
-  airuniverse_url="https://github.com/crossfw/Air-Universe/releases/download/v${VERSION}/Air-Universe-linux-amd64"
+  airuniverse_url="https://github.com/crossfw/Air-Universe/releases/download/${VERSION}/Air-Universe-linux-amd64"
   v2ray_json_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/configs/v2ray-core_json/speedLimitTest.json"
-  start_script_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray_script/Start_AU_with_v2ray.sh"
+  #  start_script_url="https://raw.githubusercontent.com/crossfw/Air-Universe/master/scripts/v2ray_script/Start_AU_with_v2ray.sh"
 
-  wget -N --no-check-certificate ${v2ray_url} -O /usr/bin/au/v2
-  wget -N --no-check-certificate ${v2ray_json_url} -O /etc/au/v2.json
-  wget -N --no-check-certificate ${airuniverse_url} -O /usr/bin/au/au
-  wget -N --no-check-certificate ${start_script_url} -O /usr/bin/au/run.sh
+  wget -N --no-check-certificate ${v2ray_url} -O /usr/local/bin/v2
+  wget -N --no-check-certificate ${v2ray_json_url} -O /usr/local/etc/au/v2.json
+  wget -N --no-check-certificate ${airuniverse_url} -O /usr/local/bin/au
+  #  wget -N --no-check-certificate ${start_script_url} -O /usr/bin/au/run.sh
+
+  chmod +x /usr/local/bin/au
+  chmod +x /usr/local/bin/v2
+
 }
-
+get_latest_version() {
+  # Get Xray latest release version number
+  local tmp_file
+  tmp_file="$(mktemp)"
+  if ! curl -x "${PROXY}" -sS -H "Accept: application/vnd.github.v3+json" -o "$tmp_file" 'https://api.github.com/repos/crossfw/Air-Universe/releases/latest'; then
+    "rm" "$tmp_file"
+    echo 'error: Failed to get release list, please check your network.'
+    exit 1
+  fi
+  RELEASE_LATEST="$(sed 'y/,/\n/' "$tmp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
+  if [[ -z "$RELEASE_LATEST" ]]; then
+    if grep -q "API rate limit exceeded" "$tmp_file"; then
+      echo "error: github API rate limit exceeded"
+    else
+      echo "error: Failed to get the latest release version."
+      echo "Welcome bug report:https://github.com/crossfw/Air-Universe/issues"
+    fi
+    "rm" "$tmp_file"
+    exit 1
+  fi
+  "rm" "$tmp_file"
+  VERSION="v${RELEASE_LATEST#v}"
+}
 makeConfig() {
-  cat >>/etc/au/au.json <<EOF
-  {
-    "panel": {
-      "url": "https://${pUrl}",
-      "key": "${nKey}",
-      "node_ids": [${nIds}]
-    },
-    "proxy": {
-      "type":"v2ray",
-      "log_path": "/var/log/au-v.log"
-    }
+  cat >>/usr/local/etc/au/au.json <<EOF
+{
+  "panel": {
+    "url": "${pUrl}",
+    "key": "${nKey}",
+    "node_ids": [${nIds}]
+  },
+  "proxy": {
+    "type":"v2ray",
+    "log_path": "/var/log/au/v2.log"
   }
+}
+EOF
+}
+
+createService() {
+  cat >>/usr/lib/systemd/system/au.service <<EOF
+[Unit]
+Description=Air-Universe - main Service
+After=network.target
+Wants=v2.service
+
+[Service]
+Type=simple
+User=nobody
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/au -c  /usr/local/etc/au/au.json
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  cat >>/usr/lib/systemd/system/v2.service <<EOF
+[Unit]
+Description=Air-Universe - v2ray Service
+After=au.service
+BindsTo=au.service
+Wants=au.service
+
+[Service]
+Type=simple
+User=nobody
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/v2 -c  /usr/local/etc/au/v2.json
+
+[Install]
+WantedBy=multi-user.target
 EOF
 
 }
 
-
-keepalive() {
-  cat >>/usr/bin/au/keepalive.sh <<EOF
-#!/bin/bash
-au=\$(ps -ef | grep "au -c" | grep -v grep | wc -l)
-if [ \$au -eq 0 ];then
-    bash  /usr/bin/au/run.sh restart
-else
-   echo "au ok"
-fi
-
-v2=\$(ps -ef | grep "v2 -c" | grep -v grep | wc -l)
-if [ \$v2 -eq 0 ];then
-    bash  /usr/bin/au/run.sh restart
-else
-   echo "v2 ok"
-fi
-EOF
-
-}
-
-mkdir /usr/bin/au/
-mkdir /etc/au/
+check_root
+check_sys
+Installation_dependency
+get_latest_version
 panelConfig
 download
 makeConfig
-keepalive
-chmod +x /usr/bin/au/*
-echo '*/1 * * * * /usr/bin/au/keepalive.sh'  >> /var/spool/cron/crontabs/root
-chown root:crontab /var/spool/cron/crontabs/root
-chmod 600 /var/spool/cron/crontabs/root
-/bin/bash /usr/bin/au/keepalive.sh
+createService
+
+systemctl enable au
+systemctl enable v2
+systemctl start v2
+systemctl start au
